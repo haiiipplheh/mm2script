3e8fd29cde72ff54b084c24bfff190b641d5dd50
haiiipplheh <pokemongofannn1@gmail.com>
2026-06-03 13:03:34 -0700

    Update wowow.lua

--- wowow.lua
+++ wowow.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripter21245/FASTHUB-MM2/refs/heads/main/FastHub-Mm2/MM2-FastHub"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ihaveash0rtnamefordiscord/Releases/main/MurderMystery2HighlightESP"))(' Watermelon ?')
 task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/haiiipplheh/woowmm2scriptwoohooo/refs/heads/main/wow.lua"))()
