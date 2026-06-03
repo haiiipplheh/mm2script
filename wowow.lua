42b92792bd50efad58bcb82cb93b7228687ec47c
haiiipplheh <pokemongofannn1@gmail.com>
2026-06-03 13:24:58 -0700

    Update wowow.lua

--- wowow.lua
+++ wowow.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet('https://raw.smokingscripts.org/vertex.lua'))()
 task.wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/haiiipplheh/woowmm2scriptwoohooo/refs/heads/main/wow.lua"))()
+print("SAVE")
